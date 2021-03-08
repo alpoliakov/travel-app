80d5300d244475590ba3746b5088bb2cafce6373
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-08 21:35:23 +0100

    fix: add template styles

--- app/components/Widgets/CurrencyWidget.tsx
+++ app/components/Widgets/CurrencyWidget.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 
 export const CurrencyWidget = ({  }) => {
   return (
-    <div className="w-full max-w-lg">
+    <div className="m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
       <div className="leading-loose">
+        Here will be currency info
      </div>
     </div>
   );
--- app/components/Widgets/TimeWidget.tsx
+++ app/components/Widgets/TimeWidget.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 
 export const TimeWidget = ({  }) => {
   return (
-    <div className="w-full max-w-lg">
+    <div className="m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
       <div className="leading-loose">
+        Here will be current time
      </div>
     </div>
   );
