035b53e590b73050135c53f15dc13fecac7f3922
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-08 16:30:07 +0100

    feat: add widget, will be replaced to country-page

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Footer from './Footer';
 import Header from './Header';
+import { Widgets } from './Widgets/Widgets';
 
 export default function Layout({ children }) {
   return (
@@ -12,6 +13,8 @@ export default function Layout({ children }) {
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       </Head>
       <Header />
+      {/* widgets will be added to country-page and will be removed from here */}
+      <Widgets /> 
       <main className="main">{children}</main>
       <Footer />
     </div>
