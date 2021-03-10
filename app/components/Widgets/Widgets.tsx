b6e294be028939151d244d3ba74120794ec5637e
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-10 13:22:08 +0100

    feat: add props to timer widget

--- app/components/Widgets/Widgets.tsx
+++ app/components/Widgets/Widgets.tsx
@@ -1,23 +1,17 @@
-import React, { useState} from 'react';
-import {WeatherWidget} from './WeatherWidget';
-import {CurrencyWidget} from './CurrencyWidget';
-import {TimeWidget} from './TimeWidget';
+import React, { useState } from 'react';
+import { WeatherWidget } from './WeatherWidget';
+import { CurrencyWidget } from './CurrencyWidget';
+import { TimeWidget } from './TimeWidget';
 
 // will be removed, when widget will be added to country-page
-const PLACES = [
-  { name: "London"},
-  { name: "Berlin"},
-  { name: "Madrid" },
-  { name: "Rome" },
-];
+const PLACES = [{ name: 'London' }, { name: 'Berlin' }, { name: 'Madrid' }, { name: 'Rome' }];
 
 export const Widgets = () => {
-
   return (
     <div className="widget-container">
-        <WeatherWidget countryCapital={PLACES[0].name} />
-        <CurrencyWidget />
-        <TimeWidget /> 
+      <WeatherWidget countryCapital={PLACES[0].name} />
+      <CurrencyWidget />
+      <TimeWidget timeZone={'EST'} />
     </div>
   );
-          }
+};
