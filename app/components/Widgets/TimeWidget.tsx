9f6747fe98233666d39eed7be781a045f3a0a8e6
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-10 13:21:09 +0100

    feat: create timer widget

--- app/components/Widgets/TimeWidget.tsx
+++ app/components/Widgets/TimeWidget.tsx
@@ -1,12 +1,61 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { useIntl } from 'react-intl';
 
-export const TimeWidget = ({  }) => {
+export const TimeWidget = ({ timeZone }) => {
+  const { formatMessage: f, locale } = useIntl();
+  const [todayTime, setTodayTime] = useState(
+    new Date().toLocaleTimeString(`${locale}`, {
+      timeZone: `${timeZone}`,
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    }),
+  );
+  const [todayDate, setTodayDate] = useState(
+    new Date().toLocaleDateString(`${locale}`, {
+      timeZone: `${timeZone}`,
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+    }),
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTodayTime(
+        new Date().toLocaleTimeString(`${locale}`, {
+          timeZone: `${timeZone}`,
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+        }),
+      );
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(
+    () =>
+      setTodayDate(
+        new Date().toLocaleDateString(`${locale}`, {
+          timeZone: `${timeZone}`,
+          weekday: 'long',
+          month: 'long',
+          day: 'numeric',
+        }),
+      ),
+    [locale],
+  );
+
   return (
     <div className="m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
-      <div className="leading-loose">
-        Here will be current time
-     </div>
+      <p className="text-white font-medium text-center text-lg font-bold uppercase">
+        {todayDate ? todayDate : f({ id: 'load' })}
+      </p>
+      <p className="m-2 weather-info block text-sm text-center text-white">
+        {todayTime ? todayTime : f({ id: 'load' })}
+      </p>
     </div>
   );
 };
-
