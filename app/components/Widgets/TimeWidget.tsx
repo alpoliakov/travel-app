7e822757562098ad173644a2ea93d6ba0157fd6a
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-11 13:51:31 +0100

    refactor: change states

--- app/components/Widgets/TimeWidget.tsx
+++ app/components/Widgets/TimeWidget.tsx
@@ -3,61 +3,50 @@ import { useIntl } from 'react-intl';
 
 export const TimeWidget = ({ timeZone }) => {
   const { formatMessage: f, locale } = useIntl();
-  const [todayTime, setTodayTime] = useState(
-    new Date().toLocaleTimeString(`${locale}`, {
-      timeZone: `${timeZone}`,
-      hour12: false,
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-    }),
-  );
-  const [todayDate, setTodayDate] = useState(
-    new Date().toLocaleDateString(`${locale}`, {
+  const [todayTime, setTodayTime] = useState(null);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTodayTime(Date.now());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => setTodayTime(Date.now()), [locale]);
+
+  const timer = new Date(todayTime).toLocaleTimeString(`${locale}`, {
+    timeZone: `${timeZone}`,
+    hour12: false,
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  });
+
+  const [dayOfWeek, date] = new Date(todayTime)
+    .toLocaleTimeString(`${locale}`, {
       timeZone: `${timeZone}`,
       weekday: 'long',
       month: 'long',
       day: 'numeric',
-    }),
-  );
+    })
+    .split(',');
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTodayTime(
-        new Date().toLocaleTimeString(`${locale}`, {
-          timeZone: `${timeZone}`,
-          hour12: false,
-          hour: 'numeric',
-          minute: 'numeric',
-          second: 'numeric',
-        }),
-      );
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-  useEffect(
-    () =>
-      setTodayDate(
-        new Date().toLocaleDateString(`${locale}`, {
-          timeZone: `${timeZone}`,
-          weekday: 'long',
-          month: 'long',
-          day: 'numeric',
-        }),
-      ),
-    [locale],
-  );
+  if (!todayTime) {
+    return (
+      <div className="m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
+        {f({ id: 'load' })}...
+      </div>
+    );
+  }
 
   return (
     <div className="m-4 p-10 bg-white bg-opacity-25 rounded shadow-xl">
       <p className="text-white font-medium text-center text-lg font-bold uppercase">
-        {todayDate ? todayDate : f({ id: 'load' })}
+        {dayOfWeek}
+        <br />
+        {date}
       </p>
-      <p className="m-2 weather-info block text-sm text-center text-white">
-        {todayTime ? todayTime : f({ id: 'load' })}
-      </p>
+      <p className="m-2 weather-info block text-sm text-center text-white">{timer}</p>
     </div>
   );
 };
