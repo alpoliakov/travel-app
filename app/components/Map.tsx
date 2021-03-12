ddb69660d0c45308512ce90730e1f1124ef4be14
Sergey Klimov <desire0417@gmail.com>
2021-03-12 12:37:53 +0300

    fix: eslint comb work file

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,58 +1,53 @@
-import React from 'react'
-import {GoogleMap, Marker, LoadScript} from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React from 'react';
+
 import mapStyles from '../styles/map-styles';
 
 const containerStyle = {
-    width: '100%',
-    height: '400px'
+  width: '100%',
+  height: '400px',
 };
 
 const options = {
-    styles: mapStyles,
-    disableDefaultUI: true,
-    zoomControl: true,
-    fullscreenControl: true,
-    streetViewControl: true,
-}
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+  fullscreenControl: true,
+  streetViewControl: true,
+};
 
-const Map = ({locale, coordinates = [52.5102, 13.3834]}) => {
+const Map = ({ locale, coordinates = [52.5102, 13.3834] }) => {
+  const [map, setMap] = React.useState(null);
 
-    const [map, setMap] = React.useState(null)
+  const onLoad = React.useCallback(function callback(map) {
+    map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json');
+    setMap(map);
+  }, []);
 
-    const onLoad = React.useCallback(function callback(map) {
-        map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json');
-        setMap(map)
-    }, [])
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
 
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
+  const [lat, lng] = coordinates;
+  const center = { lat, lng };
 
-    const [lat, lng] = coordinates;
-    const center = {lat, lng};
+  const image = '/images/pin.png';
 
-  const image = "/images/pin.png";
+  const renderMap = () => (
+    <LoadScript googleMapsApiKey={'AIzaSyCq9XpVQMtB0rLVqgjgWwQgcTJmDu5mRLc'} language={locale}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={6}
+        options={options}
+        onLoad={onLoad}
+        onUnmount={onUnmount}>
+        <Marker position={center} icon={image} />
+      </GoogleMap>
+    </LoadScript>
+  );
 
-    const renderMap = () => (
-      <LoadScript googleMapsApiKey={"AIzaSyCq9XpVQMtB0rLVqgjgWwQgcTJmDu5mRLc"} language={locale}>
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={6}
-            options={options}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-
-          >
-              <Marker position={center} icon={image} />
-          </GoogleMap>
-      </LoadScript>
-
-    );
-
-    return renderMap();
-
-
-}
+  return renderMap();
+};
 
 export default Map;
