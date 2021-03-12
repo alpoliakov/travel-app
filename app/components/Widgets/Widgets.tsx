4aa121bcd4f1d68d949dd913d3d1626b02a8d8ad
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2021-03-12 20:53:05 +0100

    fix: path props-data to widgets

--- app/components/Widgets/Widgets.tsx
+++ app/components/Widgets/Widgets.tsx
@@ -4,16 +4,13 @@ import { CurrencyWidget } from './CurrencyWidget';
 import { TimeWidget } from './TimeWidget';
 import { WeatherWidget } from './WeatherWidget';
 
-// will be removed, when widget will be added to countries-page
-const PLACES = [{ name: 'London' }, { name: 'Berlin' }, { name: 'Madrid' }, { name: 'Rome' }];
-
-export const Widgets = () => {
+export const Widgets = ({ dataCountry, currency, timeZone }) => {
   return (
     <div className="widget-box">
       <div className="widget-container">
-        <WeatherWidget countryCapital={PLACES[0].name} />
-        <CurrencyWidget currency={'USD'} />
-        <TimeWidget timeZone={'EST'} />
+        <WeatherWidget countryCapital={dataCountry.en.capital} />
+        <CurrencyWidget currency={currency} />
+        <TimeWidget timeZone={timeZone} />
       </div>
     </div>
   );
