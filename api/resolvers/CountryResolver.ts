ee5a81f63d06c7e0531735b592ed952dbb7e22c2
alpoliakov <alpoliakov73@gmail.com>
2021-03-12 17:23:56 +0200

    feat: Modernization of the request.Ð

--- api/resolvers/CountryResolver.ts
+++ api/resolvers/CountryResolver.ts
@@ -1,14 +1,12 @@
 import { ObjectId } from 'mongodb';
-import { Arg, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { Arg, Query, Resolver } from 'type-graphql';
 
 import { Country, CountryModel } from '../entity/Country';
-import { isAuth } from '../middleware/isAuth';
 import { ObjectIdScalar } from '../schema/object-id.scalar';
 
 @Resolver(() => Country)
 export class CountryResolver {
   @Query(() => Country, { nullable: true })
-  @UseMiddleware(isAuth)
   country(@Arg('countryId', () => ObjectIdScalar) countryId: ObjectId) {
     return CountryModel.findById(countryId);
   }
